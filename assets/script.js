0a5433e402dc0477d9f050214239680b34e74c27
Juliarose <juliarosewhitten@gmail.com>
2023-05-12 17:25:10 +1000

    Adding JS script

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,24 @@
 var topics = ['HTML', 'CSS', 'Git', 'Javascript'];
-
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+function ListTopics() {
 for (var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
+  }
 }
-/*
-if (topic === 'HTML') {
+function selectTopic() {
+if (randomTopic === 'HTML') {
     console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
     console.log("Let's study CSS!");
-}else if (topic === 'Git') {
+} else if (randomTopic === 'Git') {
     console.log("Let's study Git!");
-}else if (topic === 'Javascript') {
+} else if (randomTopic === 'Javascript') {
     console.log("Let's study Javascript!");
 } else {
     console.log('Please try again!');
+  }
 }
-*/
+console.log('Here are the topics we learned through Prework:');
+ListTopics();
+console.log('which topic should we study first?');
+selectTopic();
